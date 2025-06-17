470c48f8d49ace1654d785e02b1ef5414d82f259
Egor <fisher12235@mail.ru>
2025-06-17 11:26:33 +0700

    chat model

--- app/chat/models.py
+++ app/chat/models.py
@@ -1,2 +1,12 @@
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm
+from sqlalchemy import ForeignKey,Text
+from sqlalchemy.orm import Mapped,mapped_column
+from app.database import Base
+
+
+class Message(Base):
+    __tablename__ = 'messages'
+
+    id: Mapped[int] = mapped_column(primary_key=True,index=True)
+    sender_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    recipient_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    content: Mapped[str] = mapped_column(Text)
